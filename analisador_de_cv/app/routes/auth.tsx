8d43745dc19969ef5c8f660d64a1ee364c1f22c4
Mércio <mercio20a@gmail.com>
2025-09-09 19:58:45 +0100

    auth.tsx

--- analisador_de_cv/app/routes/auth.tsx
+++ analisador_de_cv/app/routes/auth.tsx
@@ -6,7 +6,18 @@ export const meta =()=>([
 ])
 
 const Auth=()=>{
-    return (<div>Auth</div>)
+    return (
+        <main className="bg-[url('/imagens/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center ">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Bem Vindo(a)</h1>
+                        <h2>Entre para continuar a sua Jornada Laboral</h2>
+                    </div>
+                </section>
+            </div>
+        </main>
+)
 }
 
 export default Auth

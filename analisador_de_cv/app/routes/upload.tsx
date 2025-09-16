33d74d43724766e9d590d177552767ff25b9eb8b
Mércio <mercio20a@gmail.com>
2025-09-16 22:30:35 +0100

    formulario

--- analisador_de_cv/app/routes/upload.tsx
+++ analisador_de_cv/app/routes/upload.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {useState} from 'react';
 import Navbar from "~/components/Navbar";
 
 const Upload=()=>{
+const [isProcessing,setIsProcessing]= useState();
+const [statusText,setStatusText]= useState('');
+handleSubmit=(e: FormEvent<HTMLFormElement>)=>{}
+
     return(
         <main className="bg-[url('/imagens/bg-main.svg')] bg-cover">
     <Navbar/>
-    <section className="main-section"></section>
+    <section className="main-section">
+        <div className="page-heading">
+            <h1>Feedback para o seu Emprego de Sonho!</h1>
+            {isProcessing ? (
+                <>
+                <h2>{statusText}</h2>
+                <img src="/imagens/resume-scan.gif" className="w-full">
+                </>
+            ) : (<h2>Deixe o seu Curriculo para pontuação ATS e dicas de melhoramento</h2>)}
+            {!isProcessing && (
+                <form >
+
+                </form>
+            )}
+        </div>
+    </section>
     </main>
     )
 }

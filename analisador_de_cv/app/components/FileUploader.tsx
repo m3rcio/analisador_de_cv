6ac96fcb53df0b0c063a1686ed83b9f713d525fb
Mércio <mercio20a@gmail.com>
2025-09-19 22:30:57 +0100

    dropzone properties

--- analisador_de_cv/app/components/FileUploader.tsx
+++ analisador_de_cv/app/components/FileUploader.tsx
@@ -4,8 +4,21 @@ import {useDropzone} from 'react-dropzone';
 
 
 const FileUploader=()=>{
+
+      const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file = acceptedFiles[0] || null;
+
+        onFileSelect?.(file);
+    }, [onFileSelect]);
+
+    const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({
+        onDrop,
+        multiple: false,
+        accept: { 'application/pdf': ['.pdf']},
+        maxSize: maxFileSize,
+    })
     return(
-        <div className="w-full gradient-Border">FileUploader</div>
+        <div className="w-full gradient-border">FileUploader</div>
     )
 }
 
